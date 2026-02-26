f8c89ee0ee8876d8a2f6119946f6d21abe7bcba3
raees03730 <ransari60212@gmail.com>
2026-02-26 17:43:06 +0530

    fixed App.jsx and home folder structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Courses from './courses/Courses';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Contact from './components/Contact';
+
 import { Toaster } from 'react-hot-toast';
 import { UseAuth } from './context/AuthProvider';
 
@@ -18,10 +19,11 @@ function App() {
      <div className="dark:bg-slate-900 dark:text-white">
        <Routes>
          <Route path="/" element={<Home/>}/>
-         <Route path="/course" element={authUser?<Courses/>:<Navigate to="/signup"/>} />
+         <Route path="/course" element={authUser ? <Courses /> : <Navigate to="/signup" />} />
          <Route path="/signup" element={<Signup/>}/>
          <Route path="/login" element={<Login/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        
         </Routes>
         <Toaster />
       </div>
