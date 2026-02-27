67899c62428a84b2e5147d46e01048b9b7952f7d
raees03730 <ransari60212@gmail.com>
2026-02-27 10:46:52 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
    useEffect(() =>{
       const getBook=async()=>{
          try {
-          const res = await axios.get("https://bookhub-backend-g1iq.onrender.com/book");
+          const res = await axios.get("https://bookhub-backend-zzvd.onrender.com/book");
           console.log(res.data);
           setBook(res.data);
          } catch (error) {
