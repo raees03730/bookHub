92530334b7e46c1d3d4e134a23705a31019f85a5
raees03730 <ransari60212@gmail.com>
2026-02-27 10:47:32 +0530

    Update API endpoint for fetching free books

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ function Freebook() {
    useEffect(() =>{
       const getBook=async()=>{
          try {
-          const res = await axios.get("https://bookhub-backend-g1iq.onrender.com/book");
+          const res = await axios.get("https://bookhub-backend-zzvd.onrender.com/book");
       
           const data = res.data.filter((data) => data.category === "Free")
           console.log(data);
